bd65398acf9682fbe4c43874becf27a5c334d0b1
unknown <initrunlevel0@gmail.com>
2012-12-29 09:38:04 +0700

    modified rentalin.csproj

--- Rentalin/frmTambahKoleksi.Designer.cs
+++ Rentalin/frmTambahKoleksi.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.lblTambahKoleksi = new System.Windows.Forms.Label();
-            this.lblKodeMember = new System.Windows.Forms.Label();
+            this.lblKode = new System.Windows.Forms.Label();
             this.txtKode = new System.Windows.Forms.TextBox();
-            this.lblNamaMember = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
             this.txtJudul = new System.Windows.Forms.TextBox();
             this.lblDeskripsi = new System.Windows.Forms.Label();
             this.txtDeskripsi = new System.Windows.Forms.TextBox();
-            this.lblJenisIdentitas = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
             this.cmbKategori = new System.Windows.Forms.ComboBox();
             this.btnKategori = new System.Windows.Forms.Button();
             this.lblJenis = new System.Windows.Forms.Label();
@@ -52,12 +52,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.lblHargaSewa = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.lblHargaDenda = new System.Windows.Forms.Label();
-            this.txtHargaSewa = new System.Windows.Forms.TextBox();
-            this.txtHargaDenda = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picCoverArt)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,12 +66,12 @@
             // 
             // lblKode
             // 
-            this.lblKodeMember.AutoSize = true;
-            this.lblKodeMember.Location = new System.Drawing.Point(7, 60);
-            this.lblKodeMember.Name = "lblKode";
-            this.lblKodeMember.Size = new System.Drawing.Size(69, 13);
-            this.lblKodeMember.TabIndex = 1;
-            this.lblKodeMember.Text = "Kode Koleksi";
+            this.lblKode.AutoSize = true;
+            this.lblKode.Location = new System.Drawing.Point(7, 60);
+            this.lblKode.Name = "lblKode";
+            this.lblKode.Size = new System.Drawing.Size(69, 13);
+            this.lblKode.TabIndex = 1;
+            this.lblKode.Text = "Kode Koleksi";
             // 
             // txtKode
             // 
@@ -89,16 +83,16 @@
             // 
             // lblNama
             // 
-            this.lblNamaMember.AutoSize = true;
-            this.lblNamaMember.Location = new System.Drawing.Point(44, 82);
-            this.lblNamaMember.Name = "lblNama";
-            this.lblNamaMember.Size = new System.Drawing.Size(32, 13);
-            this.lblNamaMember.TabIndex = 3;
-            this.lblNamaMember.Text = "Judul";
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(44, 84);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(32, 13);
+            this.lblNama.TabIndex = 3;
+            this.lblNama.Text = "Judul";
             // 
             // txtJudul
             // 
-            this.txtJudul.Location = new System.Drawing.Point(89, 82);
+            this.txtJudul.Location = new System.Drawing.Point(89, 84);
             this.txtJudul.Name = "txtJudul";
             this.txtJudul.Size = new System.Drawing.Size(196, 20);
             this.txtJudul.TabIndex = 4;
@@ -106,7 +100,7 @@
             // lblDeskripsi
             // 
             this.lblDeskripsi.AutoSize = true;
-            this.lblDeskripsi.Location = new System.Drawing.Point(26, 107);
+            this.lblDeskripsi.Location = new System.Drawing.Point(26, 111);
             this.lblDeskripsi.Name = "lblDeskripsi";
             this.lblDeskripsi.Size = new System.Drawing.Size(50, 13);
             this.lblDeskripsi.TabIndex = 5;
@@ -114,33 +108,32 @@
             // 
             // txtDeskripsi
             // 
-            this.txtDeskripsi.Location = new System.Drawing.Point(89, 107);
+            this.txtDeskripsi.Location = new System.Drawing.Point(89, 111);
             this.txtDeskripsi.Multiline = true;
             this.txtDeskripsi.Name = "txtDeskripsi";
-            this.txtDeskripsi.Size = new System.Drawing.Size(196, 65);
+            this.txtDeskripsi.Size = new System.Drawing.Size(196, 100);
             this.txtDeskripsi.TabIndex = 6;
             // 
             // lblKategori
             // 
-            this.lblJenisIdentitas.AutoSize = true;
-            this.lblJenisIdentitas.Location = new System.Drawing.Point(26, 181);
-            this.lblJenisIdentitas.Name = "lblKategori";
-            this.lblJenisIdentitas.Size = new System.Drawing.Size(46, 13);
-            this.lblJenisIdentitas.TabIndex = 7;
-            this.lblJenisIdentitas.Text = "Kategori";
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(26, 223);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(46, 13);
+            this.lblKategori.TabIndex = 7;
+            this.lblKategori.Text = "Kategori";
             // 
             // cmbKategori
             // 
-            this.cmbKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbKategori.FormattingEnabled = true;
-            this.cmbKategori.Location = new System.Drawing.Point(89, 178);
+            this.cmbKategori.Location = new System.Drawing.Point(89, 220);
             this.cmbKategori.Name = "cmbKategori";
             this.cmbKategori.Size = new System.Drawing.Size(126, 21);
             this.cmbKategori.TabIndex = 8;
             // 
             // btnKategori
             // 
-            this.btnKategori.Location = new System.Drawing.Point(219, 175);
+            this.btnKategori.Location = new System.Drawing.Point(219, 217);
             this.btnKategori.Name = "btnKategori";
             this.btnKategori.Size = new System.Drawing.Size(71, 36);
             this.btnKategori.TabIndex = 9;
@@ -150,7 +143,7 @@
             // lblJenis
             // 
             this.lblJenis.AutoSize = true;
-            this.lblJenis.Location = new System.Drawing.Point(41, 206);
+            this.lblJenis.Location = new System.Drawing.Point(41, 257);
             this.lblJenis.Name = "lblJenis";
             this.lblJenis.Size = new System.Drawing.Size(31, 13);
             this.lblJenis.TabIndex = 10;
@@ -158,9 +151,8 @@
             // 
             // cmbJenis
             // 
-            this.cmbJenis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbJenis.FormattingEnabled = true;
-            this.cmbJenis.Location = new System.Drawing.Point(89, 203);
+            this.cmbJenis.Location = new System.Drawing.Point(89, 254);
             this.cmbJenis.Name = "cmbJenis";
             this.cmbJenis.Size = new System.Drawing.Size(74, 21);
             this.cmbJenis.TabIndex = 11;
@@ -224,7 +216,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(72, 82);
+            this.label2.Location = new System.Drawing.Point(72, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(15, 20);
             this.label2.TabIndex = 18;
@@ -235,7 +227,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(72, 107);
+            this.label3.Location = new System.Drawing.Point(72, 111);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(15, 20);
             this.label3.TabIndex = 19;
@@ -246,7 +238,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(72, 179);
+            this.label4.Location = new System.Drawing.Point(72, 221);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(15, 20);
             this.label4.TabIndex = 20;
@@ -257,7 +249,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(72, 204);
+            this.label5.Location = new System.Drawing.Point(72, 255);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(15, 20);
             this.label5.TabIndex = 21;
@@ -283,73 +275,11 @@
             this.label7.TabIndex = 23;
             this.label7.Text = "informasi ini dibutuhkan";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(73, 231);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(15, 20);
-            this.label8.TabIndex = 26;
-            this.label8.Text = "*";
-            // 
-            // lblHargaSewa
-            // 
-            this.lblHargaSewa.AutoSize = true;
-            this.lblHargaSewa.Location = new System.Drawing.Point(8, 233);
-            this.lblHargaSewa.Name = "lblHargaSewa";
-            this.lblHargaSewa.Size = new System.Drawing.Size(66, 13);
-            this.lblHargaSewa.TabIndex = 24;
-            this.lblHargaSewa.Text = "Harga Sewa";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(73, 257);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(15, 20);
-            this.label10.TabIndex = 29;
-            this.label10.Text = "*";
-            // 
-            // lblHargaDenda
-            // 
-            this.lblHargaDenda.AutoSize = true;
-            this.lblHargaDenda.Location = new System.Drawing.Point(3, 259);
-            this.lblHargaDenda.Name = "lblHargaDenda";
-            this.lblHargaDenda.Size = new System.Drawing.Size(71, 13);
-            this.lblHargaDenda.TabIndex = 27;
-            this.lblHargaDenda.Text = "Harga Denda";
-            // 
-            // txtHargaSewa
-            // 
-            this.txtHargaSewa.Location = new System.Drawing.Point(89, 230);
-            this.txtHargaSewa.Name = "txtHargaSewa";
-            this.txtHargaSewa.Size = new System.Drawing.Size(93, 20);
-            this.txtHargaSewa.TabIndex = 32;
-            this.txtHargaSewa.TextChanged += new System.EventHandler(this.validateTextInteger);
-            // 
-            // txtHargaDenda
-            // 
-            this.txtHargaDenda.Location = new System.Drawing.Point(89, 256);
-            this.txtHargaDenda.Name = "txtHargaDenda";
-            this.txtHargaDenda.Size = new System.Drawing.Size(93, 20);
-            this.txtHargaDenda.TabIndex = 33;
-            this.txtHargaDenda.TextChanged += new System.EventHandler(this.validateTextInteger);
-            // 
             // frmTambahKoleksi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(308, 486);
-            this.Controls.Add(this.txtHargaDenda);
-            this.Controls.Add(this.txtHargaSewa);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.lblHargaDenda);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.lblHargaSewa);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -366,13 +296,13 @@
             this.Controls.Add(this.lblJenis);
             this.Controls.Add(this.btnKategori);
             this.Controls.Add(this.cmbKategori);
-            this.Controls.Add(this.lblJenisIdentitas);
+            this.Controls.Add(this.lblKategori);
             this.Controls.Add(this.txtDeskripsi);
             this.Controls.Add(this.lblDeskripsi);
             this.Controls.Add(this.txtJudul);
-            this.Controls.Add(this.lblNamaMember);
+            this.Controls.Add(this.lblNama);
             this.Controls.Add(this.txtKode);
-            this.Controls.Add(this.lblKodeMember);
+            this.Controls.Add(this.lblKode);
             this.Controls.Add(this.lblTambahKoleksi);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -412,11 +342,5 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label lblHargaSewa;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label lblHargaDenda;
-        private System.Windows.Forms.TextBox txtHargaSewa;
-        private System.Windows.Forms.TextBox txtHargaDenda;
     }
 }
